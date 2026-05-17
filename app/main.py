9d87dc2768b1ff3f79d2de0c53d05dc70eb195b3
ledangkhanhhy <ledangkhanhhy@gmail.com>
2026-05-17 17:09:33 +0700

    chore: Remove empty __init__.py file from app/api directory

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-
+import uvicorn
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -10,9 +10,6 @@
 #
 #
 # # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-#
 # # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 from fastapi import FastAPI
@@ -20,6 +17,12 @@ from sqlalchemy import text
 
 from app.api.chat import router as chat_router
 from app.core.database import engine
+
+if __name__ == '__main__':
+    uvicorn.run("app.main:app",
+                host="localhost",
+                port=8080,
+                reload=True)
 
 app = FastAPI(
     title="AI Ecommerce Backend",
