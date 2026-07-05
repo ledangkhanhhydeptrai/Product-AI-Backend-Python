76e2b9ef986eff74e09eb292191d09bd1cdb0f53
ledangkhanhhy <ledangkhanhhy@gmail.com>
2026-07-05 11:17:55 +0700

    feat: Update AI chat functionality to use Gemini API for content generation

--- app/services/ollama_service.py
+++ app/services/ollama_service.py
@@ -1,23 +1,23 @@
-import ollama
-
-def chat_with_ai(message: str):
-    response = ollama.chat(
-        model="llama3",
-        messages=[
-            {"role": "user", "content": message}
-        ]
-    )
-
-    return response["message"]["content"]
-# from google import genai
-# import os
-#
-# client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+# import ollama
 #
 # def chat_with_ai(message: str):
-#     response = client.models.generate_content(
-#         model="gemini-2.5-flash",
-#         contents=message,
+#     response = ollama.chat(
+#         model="llama3",
+#         messages=[
+#             {"role": "user", "content": message}
+#         ]
 #     )
 #
-#     return response.text
+#     return response["message"]["content"]
+from google import genai
+import os
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+def chat_with_ai(message: str):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=message,
+    )
+
+    return response.text
